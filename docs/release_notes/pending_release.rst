6d901ff166b51ef379fafb629d1d382a35f412d1
Paul Tunison <paul.tunison@kitware.com>
2021-03-22 16:27:58 -0400

    Initial poetry support

--- docs/release_notes/pending_release.rst
+++ docs/release_notes/pending_release.rst
@@ -5,6 +5,10 @@ SMQTK-Core Pending Release Notes
 Updates / New Features
 ----------------------
 
+Misc.
+
+* Now standardize to using `Poetry`_ for environment/build/publish management.
+
 
 Fixes
 -----
@@ -13,3 +17,6 @@ Docs
 
 * Fix incorrect filepath relative to this repository in the release process
   documentation.
+
+
+.. _Poetry: https://python-poetry.org/
--- smqtk_core/__init__.py
+++ smqtk_core/__init__.py
@@ -1,11 +1,14 @@
-# REMEMBER: `distutils.version.*Version` types can be used to compare versions
-# from strings like this.
-# SMQTK prefers to use the strict numbering standard when possible.
-__version__ = "0.16.0"
-
 import abc
+import pkg_resources
+
 from .configuration import Configurable  # noqa: F401
 from .plugin import Pluggable  # noqa: F401
+
+
+# It is known that this will fail if SMQTK-Core is not "installed" in the
+# current environment. Additional support is pending defined use-case-driven
+# requirements.
+__version__ = pkg_resources.get_distribution(__name__).version
 
 
 class Plugfigurable (Pluggable, Configurable, metaclass=abc.ABCMeta):

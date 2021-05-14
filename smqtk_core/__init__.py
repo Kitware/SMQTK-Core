577e5ddf224fcdfd3939bb4976a4690040f849b8
Paul Tunison <paul.tunison@kitware.com>
2021-05-14 13:01:54 -0400

    Remove pkg_resources dep, use importlib metadata

--- smqtk_core/__init__.py
+++ smqtk_core/__init__.py
@@ -1,14 +1,7 @@
 import abc
-import pkg_resources
 
 from .configuration import Configurable  # noqa: F401
 from .plugin import Pluggable  # noqa: F401
-
-
-# It is known that this will fail if SMQTK-Core is not "installed" in the
-# current environment. Additional support is pending defined use-case-driven
-# requirements.
-__version__ = pkg_resources.get_distribution(__name__).version
 
 
 class Plugfigurable (Pluggable, Configurable, metaclass=abc.ABCMeta):

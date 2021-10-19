82bdbca7a773108999f076e8dc4e5095c85b10b7
ameya.shringi <ameya.shringi@kitware.com>
2021-10-19 18:36:38 -0400

    Add suggested fix for not a module error

--- smqtk_core/plugin.py
+++ smqtk_core/plugin.py
@@ -272,6 +272,10 @@ def discover_via_entrypoint_extensions(entrypoint_ns: str) -> Set[Type]:
                 f"An entrypoint with key '{entry_point.name}' and value "
                 f"'{entry_point.value}' did not specify a module (got an "
                 f"object of type `{type(m).__name__}` instead): {m}"
+                "A common cause for the issue is using an unsupported"
+                "entrypoint specification along the lines of "
+                "`foo = package.module:class`. Please change it to "
+                "`foo = package.module` to fix the issue."
             )
         else:
             type_set.update(_collect_types_in_module(m))

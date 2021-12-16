9154caacf22f9ec25dac7407bb492493f209be22
Josh Anderson <josh.anderson@kitware.com>
2021-12-16 11:30:19 -0600

    Fix shellcheck errors and modify new release notes file header

--- scripts/update_release_notes.sh
+++ scripts/update_release_notes.sh
@@ -9,54 +9,58 @@
 # Two git commits are created. One for the version bump and one for the new
 # release notes stub file.
 #
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_DIR="${SCRIPT_DIR}/.."
+DOCS_DIR="${PROJECT_DIR}/docs"
+RELEASE_NOTES_DIR="${DOCS_DIR}/release_notes"
 
 # Check args
-if [ $# != 1 ]
+if [ "$#" != 1 ]
 then
   echo "Please enter valid version bump type. Options: major, minor, or patch"
   exit 1
 fi
 
-if [ $1 != 'major' ] && [ $1 != 'minor' ] && [ $1 != 'patch' ]
+if [ "$1" != 'major' ] && [ "$1" != 'minor' ] && [ "$1" != 'patch' ]
 then
   echo "Please enter valid version bump type. Options: major, minor, or patch"
   exit 1
 fi
 
-RELEASE_NOTES_PATH="release_notes"
-RELEASE_TYPE=$1
+RELEASE_TYPE="$1"
+echo "Release type: ${RELEASE_TYPE}"
 
 # Update version
-poetry version $1
+poetry version "${RELEASE_TYPE}"
 
 # Get version
-VERSION=$(poetry version -s)
-
-echo "Current version: ${VERSION}"
+VERSION="$(poetry version -s)"
+VERSION_STR="v${VERSION}"
+VERSION_SEPERATOR=${VERSION_STR//?/=}
 
 # Rename release notes to current version
-cp ${RELEASE_NOTES_PATH}/pending_release.rst ${RELEASE_NOTES_PATH}/v${VERSION}.rst
+git mv "${RELEASE_NOTES_DIR}"/pending_release.rst "${RELEASE_NOTES_DIR}"/"${VERSION_STR}".rst
 
-echo "Created v${VERSION}.rst"
+# Replace pending release notes line
+sed -i "s/Pending Release Notes/${VERSION_STR}/" "${RELEASE_NOTES_DIR}"/"${VERSION_STR}".rst
+sed -i "s/=====================/${VERSION_SEPERATOR}/" "${RELEASE_NOTES_DIR}"/"${VERSION_STR}".rst
+
+echo "Moved pending release notes to ${VERSION_STR}.rst"
 
 # Add reference to new file in release_notes.rst
-echo "   release_notes/v${VERSION}" >> release_notes.rst
-
+echo "   release_notes/${VERSION_STR}" >> "${DOCS_DIR}"/release_notes.rst
 echo "Reference added to release_notes.rst"
 
-# Make new pending notes file
-echo "Pending Release Notes" > ${RELEASE_NOTES_PATH}/pending_release.rst
-echo "=====================\n" >> ${RELEASE_NOTES_PATH}/pending_release.rst
-echo "Updates / New Features" >> ${RELEASE_NOTES_PATH}/pending_release.rst
-echo "----------------------\n" >> ${RELEASE_NOTES_PATH}/pending_release.rst
-echo "Fixes" >> ${RELEASE_NOTES_PATH}/pending_release.rst
-echo "-----\n" >> ${RELEASE_NOTES_PATH}/pending_release.rst
-
 # Make git commits
-git add ../pyproject.toml
-git add ${RELEASE_NOTES_PATH}/v${VERSION}.rst
-git add release_notes.rst
+git add "${PROJECT_DIR}"/pyproject.toml
+git add "${RELEASE_NOTES_DIR}"/v"${VERSION}".rst
+git add "${DOCS_DIR}"/release_notes.rst
 git commit -m "Update version number to ${VERSION}"
 
-git add ${RELEASE_NOTES_PATH}/pending_release.rst
+# Make new pending notes file
+cp "${SCRIPT_DIR}"/.pending_notes_stub.rst "${RELEASE_NOTES_DIR}"/pending_release.rst
+
+git add "${RELEASE_NOTES_DIR}"/pending_release.rst
 git commit -m "Add pending release notes stub"

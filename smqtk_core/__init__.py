cdc342bb6208e1e6b9d55f432628c30aff1ab396
Philip Meier <github.pmeier@posteo.de>
2025-06-24 13:38:47 +0200

    cleanup

--- smqtk_core/__init__.py
+++ smqtk_core/__init__.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from .configuration import Configurable  # noqa: F401
 from .plugfigurable import Plugfigurable  # noqa: F401
 from .plugin import Pluggable  # noqa: F401
